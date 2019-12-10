a01189ba73909c6016f7af4e89e072b0db58835f
holay <stefanovich60@gmail.com>
2019-12-10 18:59:25 -0300

    Actividades listas: 1. Github_act 2. Maps_act 3. SQLite_act 4. MAIN_ACTIVITY 5. HOME_ACT 6. INFO_ACT 7. LIBROS_ACT 8. BASE_ACT ME FALTAN: 1. SEC_ACT 2. MQTT_ACT

--- app/src/main/java/chile/maps/biblioteca/GithubDos_act.java
+++ app/src/main/java/chile/maps/biblioteca/GithubDos_act.java
@@ -3,6 +3,7 @@ package chile.maps.biblioteca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -32,14 +33,33 @@ public class GithubDos_act extends AppCompatActivity {
 
 
 
-        if (spLibros2.getSelectedItem().equals("Farenheti")){
-            TvLibros.setText("el libro seleccionado es: "+lib[0]+" y el precio es: "+precio[0]);
+       /* LA MENTABLEMENTE LO TUBE QUE HACER EN UN BOTON YA QUE EN EL ONCREATE NO CAMBIABA EL TEXTVIEW
+       if (spLibros2.getSelectedItem().equals("Farenheti")){
+            TvLibros.setText("el libro seleccionado es: "+lib[2]+" y el precio es: "+precio[2]);
         }
         if (spLibros2.getSelectedItem().equals("Revival")){
             TvLibros.setText("el libro seleccionado es: "+lib[1]+" y el precio es: "+precio[1]);
         }
         if (spLibros2.getSelectedItem().equals("Tesla")){
             TvLibros.setText("el libro seleccionado es: "+lib[2]+" y el precio es: "+precio[2]);
+        }*/
+
+    }
+    public  void mostrar(View v) {
+        Bundle listaLibros = this.getIntent().getExtras();
+        String[] lib = listaLibros.getStringArray("libros");
+
+        Bundle listaPrecio = getIntent().getExtras();
+        int[] precio = listaPrecio.getIntArray("precio");
+
+        if (spLibros2.getSelectedItem().equals("Farenheti")) {
+            TvLibros.setText("el libro seleccionado es: " + lib[0] + " y el precio es: " + precio[0]);
+        }
+        if (spLibros2.getSelectedItem().equals("Revival")) {
+            TvLibros.setText("el libro seleccionado es: " + lib[1] + " y el precio es: " + precio[1]);
+        }
+        if (spLibros2.getSelectedItem().equals("Tesla")) {
+            TvLibros.setText("el libro seleccionado es: " + lib[2] + " y el precio es: " + precio[2]);
         }
     }
 }
